63e25206dfc4be79521fd52a5b95db4710df76f0
Lyle Resnick <lyle@cellarpoint.com>
2016-12-05 19:06:34 -0500

    fixed heights refactored append

--- app/src/main/java/com/cellarpoint/androidreportlistdemo/AccountDetailsCreditCardTransactionsFragment.java
+++ app/src/main/java/com/cellarpoint/androidreportlistdemo/AccountDetailsCreditCardTransactionsFragment.java
@@ -13,8 +13,6 @@ import java.util.List;
 
 public class AccountDetailsCreditCardTransactionsFragment extends Fragment implements AccountDetailsTransactionsList.Initializer {
 
-    private final String TAG = "AccntDtlsCrdtCrdTrnsActn";
-
     public AccountDetailsCreditCardTransactionsFragment() {
         // Required empty public constructor
     }
@@ -37,20 +35,19 @@ public class AccountDetailsCreditCardTransactionsFragment extends Fragment imple
 
     public void initializeAdapter(AccountDetailsTransactionListAdapter adapter) {
 
-        appendAuthorizedTransactions(adapter);
-        appendPostedTransactions(adapter);
+        appendTransactions(adapter, TransactionModel.authorizedData, getResources().getString(R.string.authorized_transactions), getResources().getString(R.string.authorized_transactions_not_available ) );
+        appendTransactions(adapter, null, getResources().getString(R.string.posted_transactions), getResources().getString(R.string.posted_transactions_not_available ) );
     }
 
-    private void appendAuthorizedTransactions( AccountDetailsTransactionListAdapter adapter) {
+    private void appendTransactions( AccountDetailsTransactionListAdapter adapter, List<TransactionModel> transactions, String headerText, String noTransText) {
 
 
-        List<TransactionModel> transactions = TransactionModel.authorizedData;
 
-        adapter.appendHeaderRow( getResources().getString(R.string.authorized_transactions) );
+        adapter.appendHeaderRow( headerText );
 
         if( transactions == null ) {
 
-            adapter.appendMessageRow(getResources().getString(R.string.authorized_transactions_not_available));
+            adapter.appendMessageRow(noTransText);
             return;
         }
 
@@ -61,9 +58,7 @@ public class AccountDetailsCreditCardTransactionsFragment extends Fragment imple
         while( curTransaction != null ) {
 
             String curDate = curTransaction.date;
-
             adapter.appendSubHeaderRow(curDate);
-
             while ((curTransaction != null) && curTransaction.date.equals(curDate)) {
 
                 BigDecimal amountBigDec = new BigDecimal(curTransaction.amount);
@@ -74,59 +69,12 @@ public class AccountDetailsCreditCardTransactionsFragment extends Fragment imple
                     total = total.add(amountBigDec);
                 }
                 adapter.appendDetailRow(curTransaction.description, curTransaction.amount, curTransaction.debit );
-
                 curTransaction = ( i < transactions.size() ) ? transactions.get( i++ ) : null;
             }
             adapter.appendSubFooterRow();
         }
         adapter.appendTotalRow(total.toString());
     }
-
-
-    private void appendPostedTransactions( AccountDetailsTransactionListAdapter adapter) {
-
-        List<TransactionModel> transactions = TransactionModel.postedData;
-
-
-        adapter.appendHeaderRow(getResources().getString(R.string.posted_transactions) );
-
-        if( transactions == null ) {
-
-            adapter.appendMessageRow(getResources().getString(R.string.posted_transactions_not_available));
-            return;
-        }
-
-        int i = 0;
-        TransactionModel curTransaction = ( i < transactions.size() ) ? transactions.get( i++ ) : null;
-        BigDecimal total = new BigDecimal(0.0 );
-
-        while( curTransaction != null ) {
-
-            String curDate = curTransaction.date;
-
-            adapter.appendSubHeaderRow(curDate);
-
-            while ((curTransaction != null) && curTransaction.date.equals(curDate)) {
-
-                BigDecimal amountBigDec = new BigDecimal(curTransaction.amount);
-                if (!curTransaction.debit.equals("D")) {
-                    total = total.subtract(amountBigDec);
-                }
-                else {
-                    total = total.add(amountBigDec);
-                }
-                adapter.appendDetailRow(curTransaction.description, curTransaction.amount, curTransaction.debit );
-
-                curTransaction = ( i < transactions.size() ) ? transactions.get( i++ ) : null;
-            }
-            adapter.appendSubFooterRow();
-        }
-        adapter.appendTotalRow(total.toString());
-    }
-
-
-
-
 }
 
 
